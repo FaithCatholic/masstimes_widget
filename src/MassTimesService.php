302c6df1aff7647428a341f8e9f674796a872fc7
Ariel Barreiro <abarrei@gmail.com>
2026-08-13 17:16:49 -0300

    style: fix Drupal coding standards violations

--- src/MassTimesService.php
+++ src/MassTimesService.php
@@ -6,17 +6,37 @@ use GuzzleHttp\ClientInterface;
 use Drupal\Component\Serialization\Json;
 
 /**
- *
+ * Queries the MassTimes API for parishes near a set of coordinates.
  */
 class MassTimesService {
+
+  /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
   protected ClientInterface $http;
 
+  /**
+   * Constructs a MassTimesService object.
+   *
+   * @param \GuzzleHttp\ClientInterface $http
+   *   The HTTP client.
+   */
   public function __construct(ClientInterface $http) {
     $this->http = $http;
   }
 
   /**
    * Fetch nearest parishes from the API.
+   *
+   * @param float $lat
+   *   The latitude to search around.
+   * @param float $lon
+   *   The longitude to search around.
+   *
+   * @return array
+   *   The decoded API response.
    */
   public function fetchParishes(float $lat, float $lon): array {
     $url = "https://apiv4.updateparishdata.org/Churchs/?lat={$lat}&long={$lon}&pg=1";
